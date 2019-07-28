86afd027cbe5684e0055286b3695443f3b8f5579
Kateryna Sidorenko <katerinaodua@gmail.com>
2019-07-28 22:47:36 +0300

    Second homework done

---
+++ Homework_02/Task_12.py
@@ -0,0 +1,7 @@
+# Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!)
+# Первая строка содержит следующее значение, а вторая строка содержит предыдущее значение введёного числа
+
+n = int(input('Please enter an integer number: '))
+
+print('The next number for the number ', n, ' is ', n + 1, '.', sep='')
+print('The previous number for the number ', n, ' is ', n - 1, '.', sep='')
